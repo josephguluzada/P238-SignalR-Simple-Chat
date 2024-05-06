572b7124ab321a604748931ba35b80776320cee3
josephguluzada <yquluzade98@gmail.com>
2024-05-06 13:19:28 +0400

    added files

---
+++ P238SignalR/P238SignalR/Hubs/ChatHub.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace P238SignalR.Hubs
+{
+    public class ChatHub : Hub
+    {
+        private static int Counter = 0;
+
+        public async Task SendMessageAsync(string userName, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage",userName, message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            Counter++;  
+            await Clients.All.SendAsync("UserCounter", Counter);
+
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception? exception)
+        {
+            Counter--;
+            await Clients.All.SendAsync("UserCounter", Counter);
+
+            await base.OnDisconnectedAsync(exception);
+        }
+    }
+}
---
+++ P238SignalR/P238SignalR/Program.cs
@@ -0,0 +1,37 @@
+using P238SignalR.Hubs;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(opt =>
+{
+    opt.AddDefaultPolicy(policy =>
+    {
+        policy.AllowCredentials();
+        policy.AllowAnyHeader();
+        policy.AllowAnyMethod();
+        policy.SetIsOriginAllowed(x => true);
+    });
+});
+builder.Services.AddSignalR();
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+app.UseCors();
+app.MapControllers();
+app.MapHub<ChatHub>("/chat");
+app.Run();
